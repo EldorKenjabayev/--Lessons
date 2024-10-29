586bc7b13866766ca76ea28d2cf28a4d3cb129ae
Eldor <130180629+EldorKenjabayev@users.noreply.github.com>
2024-10-29 22:37:13 +0500

    My first programm in C#

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "1.9";
+
+            //double a =  Convert.ToDouble(str, numberFormatInfo);
+            //Console.WriteLine(a);
+
+            //string str = "5.9";
+            //NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            //{
+            //    NumberDecimalSeparator = ".",
+            //};
+
+            //double a = double.Parse(str, numberFormatInfo);
+            //Console.WriteLine(a);
+            //string str = "5";
+            //int a;
+            //try
+            //{ 
+            //double a  = double.Parse(str, numberFormatInfo);
+            //  Console.WriteLine(a);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Ошибк при конветация");
+            //}
+            //bool result = int.TryParse(str, out a);
+            //if(result)
+            //{
+            //    Console.WriteLine("Succes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Failed");
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+}
